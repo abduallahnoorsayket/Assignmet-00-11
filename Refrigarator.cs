ee62b28ea2d1ec1bfc196de82464e921653173c3
abdullahnoorsayket <42333253+abduallahnoorsayket@users.noreply.github.com>
2019-10-17 00:03:50 +0600

    Add files via upload

---
+++ Refrigarator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+   class Refrigarator
+    {
+        private double MaxWeight;
+      
+
+        public Refrigarator (double maxweight)
+        {
+            this.MaxWeight = maxweight;
+        }
+        
+        List<int> NoItems = new List<int>();
+          public int NoItem {  set; get; }
+        //public int NoItem { private get; set; }
+
+        List<double> UnitWeights = new List<double>();
+        public double UnitWeight { set; get; }
+
+
+
+       /* public bool OverFlow(int NoOFItem, double WeightPerItem)
+        {
+           /* bool isOverFlow = false;
+            double newWeight = (NoOFItem * WeightPerItem);
+            double newCurrentWeight = GetCurrentWeight() + newWeight;
+            if (newCurrentWeight <= GetCurrentWeight() && GetCurrentWeight() !=0 && newCurrentWeight<= MaxWeight)
+            {
+                isOverFlow = true;
+            }
+
+           return isOverFlow;
+        }
+       */
+
+        public void  Add (int NoItem , double UnitWeight)
+        {
+          //  if (OverFlow(int NoItem, double WeightPerItem)) ;
+           
+            NoItems.Add(this.NoItem);
+            UnitWeights.Add(this.UnitWeight);
+        }
+
+
+
+
+
+   
+        public double  GetCurrentWeight()
+        {
+            double CurrentWeight = 0.0;
+            for(int i=0; i< NoItems.Count(); i++ )
+            {
+                CurrentWeight += NoItems[i] * UnitWeights[i];
+               
+            }
+            return CurrentWeight;
+        }
+
+
+        public double GetRemainingWeight()
+        {
+            double RemainingWeight=this.MaxWeight - GetCurrentWeight();
+
+            return RemainingWeight;
+        }
+
+
+        public void Show()
+        {
+
+
+
+        }
+
+
+    }
+}
---
+++ RefrigeratorUi.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+
+        Refrigarator refrigarator; 
+           // = new Refrigarator();
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+
+            refrigarator = new Refrigarator( Convert.ToDouble ( maxWeightTakeTextBox.Text));
+            maxWeightTakeTextBox.Clear();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            
+            refrigarator.NoItem =Convert.ToInt32 (itemTextBox.Text);
+
+            refrigarator.UnitWeight = Convert.ToDouble(weightTextBox.Text);
+            
+
+           //efrigarator.OverFlow(refrigarator.NoItem, refrigarator.UnitWeight);
+
+              
+                  refrigarator.Add(refrigarator.NoItem, refrigarator.UnitWeight);
+
+              
+              //else
+              //{
+
+              //    MessageBox.Show(" Can not be entered Overflow !!");
+              //}
+              
+
+            currentWeightTextBox.Text = Convert.ToString( refrigarator.GetCurrentWeight());
+
+            remainingWeightTextBox.Text = Convert.ToString(refrigarator.GetRemainingWeight());
+
+
+
+
+
+
+            itemTextBox.Clear();
+            weightTextBox.Clear();
+        }
+
+
+
+
+    }
+}
